b56d51e1bd2178bfb617ba6238b6c4e3277f68cd
Brooke Hurford and Tim Durward <timothy.a.durward@gmail.com>
2016-01-27 14:17:12 -0800

    count up current score or break once roll one

--- js/master.js
+++ js/master.js
@@ -1,4 +1,7 @@
 //BDD Business Logic
+var p1DiceRollValue = 0;
+var p2DiceRollValue = 0;
+
 
 function Player(name, curScore, totalScore){
   this.name = name;
@@ -50,7 +53,7 @@ $(document).ready(function(){
   $("#startGame").click(function(){
     if($("#player1NameForm").val() === "" || $("#player2NameForm").val() === "" ){
       alert('please input fields before begining game');
-    } else{
+    } else {
       $(".startGame").hide();
 
       //start game by showing player 1 and player 2, and showing the buttons to play the game.
@@ -66,16 +69,6 @@ $(document).ready(function(){
       $("#player2Span").text(player2.name);
       $(".pigDiceGame").show();
 
-      // Disables player 2 buttons on start game load
-
-
-    //
-    // $("#p1Rollbtn").click(function() {
-    //   var p1Disabled = $("#p1Rollbtn, #p1Holdbtn").is(":disabled");
-    //   var p2Disabled = $("#p2Rollbtn, #p2Holdbtn").is(":disabled");
-    //   var player1 = new Player(player1Name, 0, 0);
-    //   var playerRoll = pigDice(dice());
-
     $("#p1Rollbtn").click(function() {
       var player1 = new Player(player1Name, 0, 0);
       var player2 = new Player(player2Name, 0, 0);
@@ -90,11 +83,13 @@ $(document).ready(function(){
             $("#p1Rollbtn").prop('disabled', true)
             $("#p2Rollbtn").prop('disabled', false)
           }
-            player1.totalScore = player1.totalScore + 100;
+            player1.totalScore = player1.totalScore + 100; {
           }
-          $(".player1-totalScore").text(player1.totalScore);
+        $(".player1-totalScore").text(player1.totalScore);
+        }
       }
     });
+
 
     $("#p2Rollbtn").click(function() {
       var player1 = new Player(player1Name, 0, 0);
@@ -104,21 +99,31 @@ $(document).ready(function(){
       var diceRoll = pigDice(dice());
 
       while (player2.totalScore < 100) {
-        debugger;
+
         if (p2Disabled === false) {
-        $(".player2-curScore").text(diceRoll);
-          if(diceRoll === 1){
+          $(".player2-curScore").text(diceRoll);
+          console.log(p2DiceRollValue += diceRoll);
+          $("#p2Holdbtn").click(function(){
             $("#p2Rollbtn").prop('disabled', true)
             $("#p1Rollbtn").prop('disabled', false)
-          }
+
+          });
+
+
+          if(diceRoll === 1){
+            // debugger;
+            $("#p2Rollbtn").prop('disabled', true)
+            $("#p1Rollbtn").prop('disabled', false)
+            break;
+          } else {
+          
             player2.totalScore = player2.totalScore + 100;
           }
-          $(".player2-totalScore").text(player2.totalScore);
-          console.log(player2.totalScore);
+        $(".player2-totalScore").text(player2.totalScore);
+          // console.log(player2.totalScore);
+        }
       }
     });
-
-
     }
   });
 });
