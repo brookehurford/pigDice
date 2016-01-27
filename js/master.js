ea2396ffa10a051f9efe9a83a512cd79316cb786
Brooke Hurford and Tim Durward <brookebhurford@gmail.com>
2016-01-27 08:37:48 -0800

    Pass first spec

--- js/master.js
+++ js/master.js
@@ -1,1 +1,20 @@
-
+var pigDice = function(num) {
+  if (num === 1) {
+    return 1;
+  }
+  else if (num === 2) {
+    return 2;
+  }
+  else if (num === 3) {
+    return 3
+  }
+  else if (num === 4) {
+    return 4
+  }
+  else if (num === 5) {
+    return 5
+  }
+  else {
+    return 6;
+  }
+};
