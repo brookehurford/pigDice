b68b4e5b457b95f567bfdba9985cec6c4a4b8fb5
Brooke Hurford and Tim Durward <timothy.a.durward@gmail.com>
2016-01-27 08:29:29 -0800

    Add first spec, not passing

--- spec/specs.js
+++ spec/specs.js
@@ -6,3 +6,9 @@
 //     expect(functionname(parameter)).to.equal('placeholder');
 //   });
 // });
+
+describe("pigDice", function() {
+  it('will give you a point total based on a random number from the dice function', function() {
+    expect(pigDice(1)).to.equal(1);
+  });
+});
