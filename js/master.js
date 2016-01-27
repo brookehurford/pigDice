f0ad1196afb50b980ba4e3deebaabf6b4181f2db
Brooke Hurford and Tim Durward <timothy.a.durward@gmail.com>
2016-01-27 09:16:52 -0800

    add prototypes for Player Construct

--- js/master.js
+++ js/master.js
@@ -1,3 +1,5 @@
+//BDD Business Logic
+
 function Player(name, curScore, totalScore){
   this.name = name;
   this.curScore = curScore;
@@ -5,29 +7,41 @@ function Player(name, curScore, totalScore){
 }
 
 
+//prototypes
+Player.prototype.addCurScore = function(){
+  return this.curScore = this.curScore + playerRoll
+}
+
+// Might not need ?
+Player.prototype.curScoreZero = function(){
+  return this.curScore = this.curScore = 0
+}
+
+//Random generate a number from 1-6
 function dice(){
   return Math.floor(Math.random() * 6) + 1;
 }
 
 
-
-var pigDice = function(num) {
-  if (num === 1) {
+var pigDice = function(roll) {
+  if (roll === 1) {
     return 1;
   }
-  else if (num === 2) {
+  else if (roll === 2) {
     return 2;
   }
-  else if (num === 3) {
+  else if (roll === 3) {
     return 3
   }
-  else if (num === 4) {
+  else if (roll === 4) {
     return 4
   }
-  else if (num === 5) {
+  else if (roll === 5) {
     return 5
   }
   else {
     return 6;
   }
 };
+
+//User Logic
